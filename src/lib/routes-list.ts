29855dfbcb584a056185261b1f4dfc8f09fd566f
re-knownout <knownout@hotmail.com>
2022-03-23 20:34:47 +0200

    Update routes list

--- src/lib/routes-list.ts
+++ src/lib/routes-list.ts
@@ -46,5 +46,5 @@ export const appRoutesList = {
  * @param root root path
  */
 export function makeRoute (path: string, root: string = serverRoot) {
-    return "http://oldagroeco.gospmr.org" + path;
+    return root + path;
 }
